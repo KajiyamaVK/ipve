3362facc0c52070287ba508b00ad1f0b5058f196
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2023-12-21 06:53:51 -0300

    Refactor middleware to remove console logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,15 +11,12 @@ export default function middleware(request: NextRequest) {
     dashboard: new URL('/dashboard', request.url),
   }
   const token = request.cookies.get('ipve_auth_token')?.value
-  console.log('ipve_auth_token', token)
-  console.log('pathname', pathname)
+
   if (!token && pathname !== '/login') {
-    console.log('url', urls.login)
     return NextResponse.redirect(new URL(urls.login, request.url))
   } else if (token && pathname === '/login') {
-    console.log('url', urls.dashboard)
     return NextResponse.redirect(new URL(urls.dashboard, request.url))
   }
-  console.log('11111111')
+
   // return NextResponse.next()
 }
