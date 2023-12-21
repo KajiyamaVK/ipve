9d95069e568fe6c8bfa8515ff8d77481ace2cc6a
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2023-12-21 06:56:39 -0300

    Refactor loginForm component and add form validation

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,19 +1,33 @@
 'use client'
-import Button from './ui/button'
+import { Button } from './ui/button'
 import { Input } from './ui/input'
-import { FormEvent } from 'react'
 import { useRouter } from 'next/navigation'
 import Cookie from 'js-cookie'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export default function LoginForm() {
+  const schema = z.object({
+    email: z
+      .string()
+      .min(1, 'O email é obrigatório.')
+      .email('O formato do email está incorreto, por favor verifique.'),
+    password: z.string().min(6, 'A senha deve ter no mínimo 6 caracteres.'),
+  })
+
   const router = useRouter()
-  function markAsLogged(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault()
+  const { register, handleSubmit, formState } = useForm({
+    resolver: zodResolver(schema),
+  })
+
+  function markAsLogged() {
     Cookie.set('ipve_auth_token', 'ajahdgfkajsdhgkh')
-    console.log('1')
+
     router.push('/dashboard')
-    console.log(2)
   }
+
+  console.log('formState', formState)
 
   return (
     <>
@@ -23,19 +37,22 @@ export default function LoginForm() {
       <form
         action=""
         className="mt-6 flex flex-col  justify-left items-left gap-2"
-        onSubmit={(e) => markAsLogged(e)}
+        onSubmit={handleSubmit(markAsLogged)}
       >
         <Input
           type="text"
-          name="email"
           placeholder="Email"
           className="focus:outline-none focus:border-b-[3px]"
+          {...register('email')}
         />
-        <Input type="password" name="senha" placeholder="Senha" />
+        <Input type="password" placeholder="Senha" {...register('password')} />
 
         <Button className="mt-6 mx-auto w-full" type="submit">
           Entrar
         </Button>
+        <pre>
+          <code>{JSON.stringify(formState, null, 2)}</code>
+        </pre>
       </form>
     </>
   )
