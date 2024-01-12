07b2798994ae0adcbb0c0adfaa980562e746e076
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-01-12 19:33:27 -0300

    updating

--- front/src/components/ui/form.tsx
+++ front/src/components/ui/form.tsx
@@ -110,7 +110,6 @@ const FormControl = React.forwardRef<
   return (
     <Slot
       ref={ref}
-      id={parseInt(formItemId)}
       aria-describedby={
         !error
           ? `${formDescriptionId}`
