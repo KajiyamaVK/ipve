a8f0dacd080e656fbe4f93a31d77851c4af94e44
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-01-25 21:01:17 -0300

    Removes Builds From Vercel Config File

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,8 +14,8 @@ app.register(people, {
 
 app
   .listen({
-    port: 3001,
+    port: process.env.PORT ? Number(process.env.PORT) : 3001,
   })
   .then(() => {
-    console.log('Server listening on port 3001')
+    console.log('Server listening')
   })
