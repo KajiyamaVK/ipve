a7956c3b2c12515ecea0457edd5ba2cccfc83304
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-01-25 21:39:57 -0300

    Changes package.json scripts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ app.register(people, {
 
 app
   .listen({
-    port: process.env.PORT ? Number(process.env.PORT) : 3001,
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
   })
   .then(() => {
     console.log('Server listening')
