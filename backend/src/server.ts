f6aeee9cde907bbe297f61a05e359f2a10202c53
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-18 16:52:55 -0300

    Teste de performance

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import fastify from 'fastify'
-// import 'dotenv/config'
-// import { generalRoutes } from './routes/general'
+//import 'dotenv/config'
+//import { generalRoutes } from './routes/general'
 import cookie from '@fastify/cookie'
 import { roles } from './routes/people/roles'
 import cors from '@fastify/cors'
@@ -14,22 +14,23 @@ import { kinsRelations } from './routes/people/kinsRelations'
 import { env } from './env'
 import { storePeoplePhoto } from './routes/people/storePhoto'
 
+// if you only pass connectionString
 declare module 'fastify' {
   interface FastifyInstance {
     mysql: MySQLPool
   }
 }
 
-const app = fastify({ logger: true })
+export const app = fastify({ logger: true })
 
-app.register(cookie) // Must be the first register in this list
+app.register(cookie) // Tem que ser o primeiro register dessa lista
 
 app.register(multipart)
 app.register(cors)
 app.register(fastifyCaching, {
   privacy: 'private',
 })
-// app.register(generalRoutes)
+//app.register(generalRoutes)
 app.register(people, {
   prefix: '/people',
 })
@@ -49,17 +50,11 @@ app.register(mySql, {
   connectionString: env.DATABASE_URL,
 })
 
-// Conditional listen for local development
-if (require.main === module) {
-  app
-    .listen({
-      host: '0.0.0.0',
-      port: env.PORT ? Number(env.PORT) : 3001,
-    })
-    .then(() => {
-      console.info('Server running locally')
-    })
-} else {
-  // Just export the app for Vercel
-  module.exports = app
-}
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT ? Number(env.PORT) : 3001,
+  })
+  .then(() => {
+    console.info('Server running')
+  })
