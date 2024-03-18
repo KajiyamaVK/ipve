a7c5e1e08fb81738e3069a84908cca1414041210
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-18 09:56:56 -0300

    Teste para reprodução de erro - connection closed.

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -15,12 +15,13 @@ export default function middleware(request: NextRequest) {
   const token = request.cookies.get('ipve_auth_token')?.value
 
   if (!token && pathname !== '/login') {
-    console.log('Finish Middleware')
+    console.log('Finish Middleware 1')
     return NextResponse.redirect(new URL(urls.login, request.url))
   } else if (token && (pathname === '/login' || pathname === '/')) {
-    console.log('Finish Middleware')
+    console.log('Finish Middleware 2')
     return NextResponse.redirect(new URL(urls.dashboard, request.url))
   }
+  console.log('Finish Middleware 3')
 
   try {
     getEnv()
