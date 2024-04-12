f0ee775c108514d63e42eae5a3ada3ad027c382b
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-12 18:11:28 -0300

    fast commit

--- src/app/(layout)/people/roles/page.tsx
+++ src/app/(layout)/people/roles/page.tsx
@@ -1,23 +1,38 @@
+'use client'
 import { DataTable } from '@/components/ui/data-table'
 import { columns } from '@/app/(layout)/people/roles/columns'
 import { DialogModalSkeleton } from './DialogModalSkeleton'
 import { DialogModal } from './DialogModal'
 import { TRoles } from '@/types/TRoles'
 import { getData } from '@/utils/fetchData'
+import { useEffect, useState } from 'react'
+import { toast } from '@/components/ui/use-toast'
 
 export default async function RolesGrid() {
-  let data: TRoles[] = []
-
-  async function retrieveData() {
-    data = await getData<TRoles[]>({
-      endpoint: 'roles',
-    }).then((data) => {
-      if (!data) return []
-      return data
-    })
-  }
-
-  await retrieveData()
+  //let data: TRoles[] = []
+  const [data, setData] = useState<TRoles[]>([])
+  useEffect(() => {
+    async function retrieveData() {
+      await getData<TRoles[]>({
+        endpoint: 'roles',
+      })
+        .then((data) => {
+          if (!data) return []
+          setData(data)
+        })
+        .catch((error) => {
+          console.error(error.message)
+          toast({
+            title: 'Error',
+            description:
+              'Um erro ocorreu ao tentar buscar os dados das funções. Por favor, tente novamente ou entre em contato com o suporte.' +
+              error.message,
+            variant: 'destructive',
+          })
+        })
+    }
+    retrieveData()
+  }, [])
 
   return (
     <center>
