ee6ef9f9bcf6c529d33db0c6ccb557e06609a7a5
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-15 03:48:14 -0300

    Refactor: Roles

--- src/app/api/people/titles/[id]/route.ts
+++
@@ -1,6 +0,0 @@
-import { getPeopleTitles } from '../functions'
-
-export async function GET(request: Request, { params }: { params: { id: number } }) {
-  const data = await getPeopleTitles(params.id)
-  return data
-}
